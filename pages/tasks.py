d892eb0f6187b788e9e08de5db149b785918907b
adm_skryabin.p <skryabin.p.n@gmail.com>
2022-11-08 10:07:26 +0300

    all stuff

--- pages/tasks.py
+++ pages/tasks.py
@@ -16,19 +16,31 @@ def request_printers():
     for printer in PrintersMain.objects.all():
         temp_info = get_full_info(printer.ip)
         if temp_info['pages'] != None:
+
             previous_note = RequestPrinters.objects.filter(printer=printer).last()
+
             changed = 0
-            if previous_note != None:
-                # if amount of toner now is bigger than previous amount then toner was changed
-                if int(previous_note) >=0:
-                    if (int(temp_info['black_toner_left']) > (15 + int(previous_note.toner_left))) and (int(temp_info['black_toner_left'] ) > 1):
-                        changed = 1
+            if previous_note is not None:
+                if previous_note.toner_left != None:
+                    # if amount of toner now is bigger than previous amount then toner was changed
+                    if int(previous_note.toner_left) >=0:
+                        if (int(temp_info['black_toner_left']) > (15 + int(previous_note.toner_left))) and (int(temp_info['black_toner_left'] ) > 1):
+                            changed = 1
 
 
 
-            db = RequestPrinters.objects.create(printer = printer,page_utility=temp_info['pages'],
-                                                toner_left = temp_info['black_toner_left'],device_name=temp_info['Device name'],
-                                                changed=changed)
-            db.save()
+                db = RequestPrinters.objects.create(printer = printer,page_utility=temp_info['pages'],
+                                                    toner_left = temp_info['black_toner_left'],device_name=temp_info['Device name'],
+                                                    changed=changed)
+                db.save()
+            else:
+                db = RequestPrinters.objects.create(printer=printer, page_utility=temp_info['pages'],
+                                                    toner_left=temp_info['black_toner_left'],
+                                                    device_name=temp_info['Device name'],
+                                                    changed=changed)
+                db.save()
+
+
+
 #
 
